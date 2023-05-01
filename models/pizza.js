380fd7a3a103e842236b5a889482abacd5f5d10a
Tre' Grisby <trewaters@hotmail.com>
2023-05-01 15:33:53 -0700

    new files with linting added

--- models/pizza.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const pizzaSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  toppings: [{topping: String}]
-});
-
-module.exports = mongoose.model("Pizza", pizzaSchema);
